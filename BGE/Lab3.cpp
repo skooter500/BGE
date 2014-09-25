acb8126ebec9c3425a4edc0f75789b54d00946cd
unknown <skooter500>
2014-09-25 19:08:37 +0100

    Set up lab 3

--- BGE/Lab3.cpp
+++ BGE/Lab3.cpp
@@ -37,24 +37,6 @@ void Lab3::Update(float timeDelta)
 {
 	static float timeToFire = 1.0f / 2.0f;
 
-	// Movement of ship1
-	if (keyState[SDL_SCANCODE_U])
-	{
-		ship1->transform->position += ship1->transform->look * speed * timeDelta;
-	}
-	if (keyState[SDL_SCANCODE_J])
-	{
-		ship1->transform->position -= ship1->transform->look * speed * timeDelta;
-	}
-	if (keyState[SDL_SCANCODE_H])
-	{
-		ship1->transform->Yaw(timeDelta * speed * speed);
-	}
-	if (keyState[SDL_SCANCODE_K])
-	{
-		ship1->transform->Yaw(-timeDelta * speed * speed);
-	}
-
 	// Movement of ship2
 	if (keyState[SDL_SCANCODE_UP])
 	{
@@ -63,57 +45,6 @@ void Lab3::Update(float timeDelta)
 	if (keyState[SDL_SCANCODE_DOWN])
 	{
 		ship2->transform->position -= ship2->transform->look * speed * timeDelta;
-	}
-	if (keyState[SDL_SCANCODE_LEFT])
-	{
-		ship2->transform->Yaw(timeDelta * speed * speed);
-	}
-	if (keyState[SDL_SCANCODE_RIGHT])
-	{
-		ship2->transform->Yaw(-timeDelta * speed * speed);
-	}
-
-	// Check Distance to ship 2
-	glm::vec3 toShip2 = ship2->transform->position - ship1->transform->position;
-	if (glm::length(toShip2) < 5)
-	{
-		PrintText("In range");
-	}
-	else
-	{
-		PrintText("Not in range");
-	}
-	// Check in front of or behind
-	toShip2 = glm::normalize(toShip2);
-	float dot = glm::dot(toShip2, ship1->transform->look);
-	if (dot < 0)
-	{
-		PrintText("Behind");
-	}
-	else
-	{
-		PrintText("In Front");
-	}
-
-	// Check in the FOV of half 45 degrees	
-	float angle = glm::acos(dot);
-	float halffov = glm::radians(45.0f) / 2.0f;
-	if (angle < halffov)
-	{
-		if (elapsed > timeToFire)
-		{
-			shared_ptr<LazerBeam> lazer = make_shared<LazerBeam>();
-			lazer->transform->position = ship1->transform->position;
-			lazer->transform->look = ship1->transform->look;
-			Attach(lazer);
-			elapsed = 0.0f;
-		}
-		
-		PrintText("In FOV");
-	}
-	else
-	{
-		PrintText("Not in FOV");
 	}
 	elapsed += timeDelta;
 
