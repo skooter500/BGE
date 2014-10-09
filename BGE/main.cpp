68959ddee12ab6fd2d560ef4ace0c6c114d4615d
unknown <skooter500>
2014-10-09 15:54:48 +0100

    Added right click phys gun

--- BGE/main.cpp
+++ BGE/main.cpp
@@ -47,9 +47,9 @@ using namespace BGE;
 int main(int argc, char *argv[])
 {
 	// http://www.youtube.com/watch?v=EEbVHxOkTxw
-	Params::Load("vrgame");
+	Params::Load("default");
 
-	shared_ptr<Game> game = make_shared<VRGame2>();
+	shared_ptr<Game> game = make_shared<SceneGraphGame>();
 	game->Run();
 	
 	return 0;
