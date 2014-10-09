9e376689d0daf00ee850437977d4690964985188
unknown <skooter500>
2014-10-09 15:32:53 +0100

    Bla

--- BGE/main.cpp
+++ BGE/main.cpp
@@ -47,9 +47,9 @@ using namespace BGE;
 int main(int argc, char *argv[])
 {
 	// http://www.youtube.com/watch?v=EEbVHxOkTxw
-	Params::Load("default");
+	Params::Load("vrgame");
 
-	shared_ptr<Game> game = make_shared<Buddha>();
+	shared_ptr<Game> game = make_shared<VRGame2>();
 	game->Run();
 	
 	return 0;
